91dbdefdc91dd2cf72f766b7b6694edf7c7058a1
Jeremy <jgomallette@gmail.com>
2018-01-13 19:56:36 -0500

    Replace settings with account and device settings

--- frontend/src/app/components/device-settings/device-settings.component.spec.ts
+++ frontend/src/app/components/device-settings/device-settings.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SettingsComponent } from './settings.component';
+import { DeviceSettingsComponent } from './device-settings.component';
 
-describe('SettingsComponent', () => {
-  let component: SettingsComponent;
-  let fixture: ComponentFixture<SettingsComponent>;
+describe('DeviceSettingsComponent', () => {
+  let component: DeviceSettingsComponent;
+  let fixture: ComponentFixture<DeviceSettingsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SettingsComponent ]
+      declarations: [ DeviceSettingsComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SettingsComponent);
+    fixture = TestBed.createComponent(DeviceSettingsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
