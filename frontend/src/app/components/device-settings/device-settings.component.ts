84f8c1646d5db73b7141a336579787ad3c819f87
Jeremy <jgomallette@gmail.com>
2018-01-29 15:28:47 -0500

    Implement collapsibles in device settings; Bug fixes

--- frontend/src/app/components/device-settings/device-settings.component.ts
+++ frontend/src/app/components/device-settings/device-settings.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-settings.component.css']
 })
 export class DeviceSettingsComponent implements OnInit {
+  headers: any;
+  panels: any;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    this.headers = document.getElementsByClassName('collapse-header')
+    this.panels = document.getElementsByClassName('collapse-panel');
+  }
+
+  expand(target) {
+    for(var i = 0; i < 3; i++) {
+      var header = this.headers[i];
+      var panel = this.panels[i];
+
+      if(i == target) {
+        header.classList.add('active-header');
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } else {
+        header.classList.remove('active-header');
+        panel.style.maxHeight = "0";
+      }
+    }
+  }
 }
